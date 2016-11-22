39b3f31c541e50c94035013c3ccb9f3669195a6f
Valtteri Ranta <valtterih.ranta@gmail.com>
2016-11-22 22:18:26 +0200

    first commit, background and scrolling team logos

--- FifaAppScrollingProto/ViewController.swift
+++ FifaAppScrollingProto/ViewController.swift
@@ -9,10 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    var images = [UIImageView]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        var contentWidth: CGFloat = 0.0
+        let scrollWidth = scrollView.frame.size.width
+        
+        for x in 0...7 {
+            let image = UIImage(named: "team\(x + 1)")
+            let imageView = UIImageView(image: image)
+            images.append(imageView)
+            
+            var newX: CGFloat = 0.0
+            
+            newX = scrollWidth / 2 + scrollWidth * CGFloat(x)
+            contentWidth = scrollWidth*CGFloat(x)
+            
+            scrollView.addSubview(imageView)
+            imageView.frame = CGRect(x: newX - 90, y: (scrollView.frame.size.height / 4) - 90, width: 180, height: 180)
+            
+            scrollView.contentSize = CGSize(width: contentWidth, height: view.frame.size.height)
+        }
     }
 
     override func didReceiveMemoryWarning() {
